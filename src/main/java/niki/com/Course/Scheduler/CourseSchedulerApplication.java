821914faac9cd65e902ccf1b02fcc2219b20fa4a
nikimohseny <mohseny_n@auca.kg>
2025-10-01 11:13:29 -0400

    first commit, commiting the project repo

---
+++ src/main/java/niki/com/Course/Scheduler/CourseSchedulerApplication.java
@@ -0,0 +1,13 @@
+package niki.com.Course.Scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSchedulerApplication.class, args);
+	}
+
+}
